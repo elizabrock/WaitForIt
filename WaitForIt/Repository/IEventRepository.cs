ef5a93c56a8182a9421bfeafe658ee42a8d51961
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-02-10 12:19:12 -0600

    1. Added interface for Event Repository with implementation stubs. 2. Added EventContext

---
+++ WaitForIt/Repository/IEventRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WaitForIt.Model;
+
+namespace WaitForIt.Repository
+{
+    public interface IEventRepository
+    {
+        int GetCount();
+        void Add();
+        void Delete();
+        void Update();
+        void Clear();
+        IQueryable<Event> PastEvents(); // EventRepository.PastEvents.Where(c => c.CalculateMonth() == 2)
+        int CalculateMonth();
+        IQueryable<Event> All();
+        Event GetById(int id);
+        Event GetByDate(string date);
+        IQueryable<Event> SearchFor(Expression<Func<Event, bool>> predicate);
+
+        
+     
+    }
+}
