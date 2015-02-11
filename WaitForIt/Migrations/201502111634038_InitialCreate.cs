8defbc8bc448965955e7906ff45bc9867f1751d8
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-02-11 12:09:55 -0600

    Migrations from class: InitialCreate and AddHashField

---
+++ WaitForIt/Migrations/201502111634038_InitialCreate.cs
@@ -0,0 +1,27 @@
+namespace WaitForIt.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Events",
+                c => new
+                    {
+                        EventId = c.Int(nullable: false, identity: true),
+                        Date = c.String(),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.EventId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Events");
+        }
+    }
+}
