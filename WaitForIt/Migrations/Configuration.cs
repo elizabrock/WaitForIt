4ab5be9acc07ade39e84a255894449e125840def
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-02-26 15:56:37 -0600

    show how to enable auto migrations

--- WaitForIt/Migrations/Configuration.cs
+++ WaitForIt/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace WaitForIt.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
             ContextKey = "WaitForIt.EventContext";
         }
 
